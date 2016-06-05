f8f7f5d86efa589c11c356bb4d805ce9e7aa245a
Raj <rajeev.v.singh@gmail.com>
2016-06-05 16:29:07 -0400

    switched to .on('click') method, checking for category by using .parents() method and appending (without cloning) to the appropriate list by adding/subtracting to an index on each list using a data-list-number attribute, added fontawesome for arrow icons

--- js/app.js
+++ js/app.js
@@ -1,65 +1,73 @@
 $(document).ready(function() {
 
-  document.getElementById("new-area1-item").addEventListener('keypress', function(e) {
-         if (e.keyCode == 13) {
-           var newItem = $("#new-area1-item").val();
-           $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-1 ul").html(newItem);
-          e.preventDefault();
-         }
+  $(".store-area").on('click', '.button', function() {
+
+    // Since this is the button, we want to work with the item as a whole when
+    // appending.
+    var item = $(this).parent('li');
+
+    // assign the category to a variable by traversing the DOM up to the nearest
+    // .store-area and getting the data-category attribute
+    var parentCategory = $(this).parents('.store-area').attr('data-category');
+
+    // assign the list number to a variable. This list number should correspond
+    // to the index of the lists array defined above.
+    var parentListIndex = parseInt( $(this).parents('.list').attr('data-list-number') );
+
+    var newIndex = parentListIndex;
+    if ( $(this).hasClass('move-left') && parentListIndex > 0) {
+      newIndex--;
+    } else if ( $(this).hasClass('move-right') && parentListIndex < 2) {
+      newIndex++;
+    }
+    item.appendTo('.list[data-list-number="' + newIndex + '"] .store-area[data-category="' + parentCategory + '"] ul');
+
+
   });
 
-  document.getElementById("new-area2-item").addEventListener('keypress', function(e) {
-         if (e.keyCode == 13) {
-           var newItem = $("#new-area2-item").val();
-           $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-2 ul");
-           console.log(newItem);
-             e.preventDefault();
-         }
-  });
+  $('input[type="submit"]').click(function() {
+    // Assign the text input to a variable. We have to do this since this event
+    // handler is bound to the submit button.
+    var textInput = $('input[name="new-area-item"]', $(this).parent('form') );
 
-  document.getElementById("new-area3-item").addEventListener('keypress', function(e) {
-         if (e.keyCode == 13) {
-           var newItem = $("#new-area3-item").val();
-           $("<li>" + newItem + "</li>").appendTo("#inventory-list .store-area-3 ul");
-           console.log(newItem);
-             e.preventDefault();
-         }
-  });
-/* if mousedown occurs in inventory-list area 1, move item to shopping-list */
-$("#inventory-list .store-area-1 li").mousedown(function() {
-  $(this).clone().appendTo("#shopping-list .store-area-1 ul")
-  $(this).toggleClass("item-selected");
-});
+    // The name of the new item to be added is the value of the text input
+    var newItemName = textInput.val();
 
-/* if mousedown occurs in inventory-list area 2, move item to shopping-list */
-$("#inventory-list .store-area-2 li").mousedown(function() {
-  $(this).clone().appendTo("#shopping-list .store-area-2 ul")
-  $(this).toggleClass("item-selected");
-});
+    // assign the category to a variable by traversing the DOM up to the nearest
+    // .store-area and getting the data-category attribute
+    var parentCategory = $(this).parents('.store-area').attr('data-category');
 
-/* if mousedown occurs in inventory-list area 3, move item to shopping-list */
-$("#inventory-list .store-area-3 li").mousedown(function() {
-  $(this).clone().appendTo("#shopping-list .store-area-3 ul")
-  $(this).toggleClass("item-selected");
-});
+    // do nothing if the new item name is empty
+    if (newItemName.length === 0) { return false; }
 
-/* if mousedown occurs in shopping-list, hide item in shopping list and show in in-cart */
-/*$("#shopping-list ul").on("click", "li", function() {
-  $(this).clone().appendTo("#in-cart ul");
-  $(this).hide();
-}); */
-
-$(function() {
-    $("#in-cart ul").sortable({
-      revert: true
+    // Iterate through the list of existing items and check for ones that have
+    // the same name. If a match is found, do nothing.
+    var itemAlreadyExists = false;
+    $('.store-area li').each(function() {
+      // Note that $(this) now refers to the li that's currently being
+      // iterated through, which is why this console log will list all items
+      // within the same .store-area container.
+      console.log ( $(this).text() );
+      if ( $(this).text() === newItemName) {
+        itemAlreadyExists = true;
+      }
     });
-    $("#shopping-list .store-area-1 ul li").draggable({
-      connectToSortable: "#in-cart ul",
-      helper: "clone",
-      revert: "invalid"
-    });
-/*    $( "ul, li" ).disableSelection(); */
-  });
+    if (itemAlreadyExists) {
+      return false;
+    }
 
+    // Dynamically create the new item element and populate it with the .text method
+    var newItem = $('<li />').text(newItemName);
+    // Create the left and right arrows, using fontawesome - http://fontawesome.io/
+    var newItemRightArrow = $('<i />').addClass('move-right button fa fa-arrow-right').appendTo(newItem);
+    var newItemLeftArrow = $('<i />').addClass('move-left button fa fa-arrow-left').prependTo(newItem);
+
+    newItem.appendTo('#inventory-list .store-area[data-category="' + parentCategory + '"] ul');
+
+    // clear the input
+    textInput.val('');
+
+    return false;
+  });
 
 });
